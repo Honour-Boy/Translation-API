ac899fee270c5a29aee66c4900ff2ab69552059a
IndyWebstars <honouradewunmi42@gmail.com>
2024-06-17 18:17:13 +0100

    New files added and changes made to app.py

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
-import os
 from flask import Flask, request, jsonify
 from transformers import MBartForConditionalGeneration, MBart50Tokenizer
 import openai
+import os
 
 app = Flask(__name__)
 
@@ -45,5 +45,4 @@ def translate():
     return jsonify({"translated_text": translated_text})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
